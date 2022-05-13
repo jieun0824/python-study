a591ef7b2c9d0487c5a7276474f5b700fb13320f
jieun1824 <suspen064@gmail.com>
2022-05-14 02:23:09 +0900

    0514

--- s2/api.py
+++ s2/api.py
@@ -1,22 +1,24 @@
-import requests
-import json
+# import requests
+# import json
 
-city = "Seoul"
-apikey = "729cb4f29b7c8b58b672389141ba2b80"
-lang = "kr"
+# city = "Seoul"
+# apikey = "729cb4f29b7c8b58b672389141ba2b80"
+# lang = "kr"
 
 api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}&lang={lang}&units=metric"
 
-result = requests.get(api)
-data = json.loads(result.text)
+# result = requests.get(api)
+# data = json.loads(result.text)
 
-print(data["name"],"의 날씨입니다.")
-print("날씨는 ",data["weather"][0]["description"],"입니다.")
-print("현재 온도는 ",data["main"]["temp"],"입니다.")
-print("하지만 체감 온도는 ",data["main"]["feels_like"],"입니다.")
-print("최저 기온은 ",data["main"]["temp_min"],"입니다.")
-print("최고 기온은 ",data["main"]["temp_max"],"입니다.")
-print("습도는 ",data["main"]["humidity"],"입니다.")
-print("기압은 ",data["main"]["pressure"],"입니다.")
-print("풍향은 ",data["wind"]["deg"],"입니다.")
-print("풍속은 ",data["wind"]["speed"],"입니다.")
+# print(data["name"],"의 날씨입니다.")
+# print("날씨는 ",data["weather"][0]["description"],"입니다.")
+# print("현재 온도는 ",data["main"]["temp"],"입니다.")
+# print("하지만 체감 온도는 ",data["main"]["feels_like"],"입니다.")
+# print("최저 기온은 ",data["main"]["temp_min"],"입니다.")
+# print("최고 기온은 ",data["main"]["temp_max"],"입니다.")
+# print("습도는 ",data["main"]["humidity"],"입니다.")
+# print("기압은 ",data["main"]["pressure"],"입니다.")
+# print("풍향은 ",data["wind"]["deg"],"입니다.")
+# print("풍속은 ",data["wind"]["speed"],"입니다.")
+
+"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
